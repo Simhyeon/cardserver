14aea0fe0d8fd0e88d94304cb660b9e187fe9bbe
육심현 <simoncreek@tutanota.com>
2021-01-04 23:28:02 +0900

    Changed test function

--- src/test.rs
+++ src/test.rs
@@ -2,17 +2,25 @@ use crate::models;
 
 #[test]
 fn function_name_test() {
-    let mut new_pool = models::CardPool::new();
+    //let mut new_pool = models::CardPool::new();
 
-    // TEST :: Print card pool
-    //for item in new_pool.cards {
-        //println!("{:?}", item);
-    //}
-    //
-    // TEST :: Pool card and check if card was successfully deleted.
-    println!("{:?}", new_pool.cards.len());
-    let new_card = new_pool.poll_card();
+    //// TEST :: Print card pool
+    ////for item in new_pool.cards {
+        ////println!("{:?}", item);
+    ////}
+    ////
+    //// TEST :: Pool card and check if card was successfully deleted.
+    //println!("{:?}", new_pool.cards.len());
+    //let new_card = new_pool.poll_card();
 
-    println!("{:?}", new_card);
-    println!("{:?}", new_pool.cards.len());
+    //println!("{:?}", new_card);
+    //println!("{:?}", new_pool.cards.len());
+
+
+    // TEST ::: Serde_Json for option
+    let result = serde_json::to_string(&models::UserRequest{action: models::PlayerAction::MESSAGE, value: Some(25)});
+
+    if let Ok(content) = result {
+        println!("{:?}", content);
+    }
 }
