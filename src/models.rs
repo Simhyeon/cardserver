a121733b8f3d702156152d8e5b96d961c083cee9
육심현 <simoncreek@tutanota.com>
2021-01-04 23:27:05 +0900

    Made Connection struct to be a composite of multiple User constructs so that modularlity can be improved + Made response and request has concrete format so that both client and server can easily parse the given data. + Made enum follows naming convention fo rust-lang.

--- src/models.rs
+++ src/models.rs
@@ -8,10 +8,7 @@ use rand::prelude::*;
 const CARD_NUMBER : usize = 14;
 
 pub struct Connection {
-    pub creator_id: String,
     pub room_id: String,
-    pub participant_id: Option<String>,
-    pub sender : mpsc::UnboundedSender<Result<Message, warp::Error>>,
     pub game: Game,
 }
 
@@ -20,13 +17,10 @@ impl Connection {
         creator_id: String, 
         room_id: String, 
         sender: mpsc::UnboundedSender<Result<Message, warp::Error>>
-        ) -> Self {
+    ) -> Self {
         Self {  
-            creator_id,
             room_id,
-            participant_id: None,
-            sender,
-            game: Game::new(),
+            game: Game::new(creator_id, sender),
         }
     }
 }
@@ -34,57 +28,147 @@ impl Connection {
 pub struct Game {
     pub turn: Turn,
     // TODO :: Refactor to use playerstat struct.
-    pub creator_cards: Vec<Card>,
-    pub participant_cards: Vec<Card>,
-
+    pub creator: User,
+    pub participant: Option<User>,
     pub card_pool : CardPool,
 }
 
 // Game related logics
 impl Game {
-    pub fn new() -> Self {
+    pub fn new(
+        cid: String, 
+        sender: mpsc::UnboundedSender<Result<Message, warp::Error>>
+    ) -> Self {
         // TODO :: Should poll cards several times.
         // before starting game.
         Self {  
             turn: Turn::CREATOR,
-            creator_cards: vec![],
-            participant_cards: vec![],
+            creator: User::new(cid, sender),
+            participant: None,
             card_pool: CardPool::new(),
         }
     }
 
-    pub fn receive_player_action(&mut self, turn: Turn, action: PlayerAction) {
+    pub fn send_message(&self, user_id: String, msg: Message) {
+        if let None = self.participant {
+            return;
+        }
+
+        // From creator to participant
+        if user_id == self.creator.id {
+            self.participant.as_ref().unwrap().sender.send(Ok(msg))
+                .expect("Failed to send message");
+        } 
+        // From participant to creator
+        else {
+            self.creator.sender.send(Ok(msg))
+                .expect("Failed to send message");
+        }
+    }
+
+    pub fn receive_player_action(&mut self, uid: &str, action: PlayerAction) {
+
+        if let None = self.participant {
+            eprintln!("Tried to retrive action while room is not complete");
+            return;
+        }
+
         // TODO :: Make it work
         // Calculate according to given player action.
         // Validate action if not then demand action again.
         match action {
-            PlayerAction::POLLCARD => {
+            PlayerAction::Pollcard => {
+                // TODO :: I'm not sure if creating json object from card array is appropriate
+                // or just try to make json object from simple array.
                 if let Some(card) = self.card_pool.poll_card() {
+                    eprintln!("Successfully polled card from cardpool");
                     // Creator's turn
-                    if let Turn::CREATOR = turn {
-                        self.creator_cards.push(card);
+                    if uid == self.creator.id {
+                        self.creator.add_card(card.clone());
+                        let res = serde_json::to_string(&ServerResponse{response_type: ResponseType::Card, value : ResponseValue::Card(card)})
+                            .expect("Failed to create json response");
+                        self.creator.sender.send( Ok(Message::text(res)) )
+                            .expect("Failed to send response");
                     }
                     // participant's turn
                     else {
-                        self.participant_cards.push(card);
+                        self.participant.as_mut().unwrap().add_card(card.clone());
+                        let cards = serde_json::to_string(&self.participant.as_ref().unwrap().stat.cards)
+                            .expect("Failed to created cards json objects");
+                        let res = serde_json::to_string(&ServerResponse{response_type: ResponseType::Card, value : ResponseValue::Card(card)})
+                            .expect("Failed to create json response");
+                        self.participant.as_ref().unwrap().sender.send( Ok(Message::text(res)) )
+                            .expect("Failed to send response");
                     }
                 } else {
-                    eprintln!("Failed poll card from card pool");
+                    eprintln!("Failed to poll card from card pool");
                 }
             }
-            _ => {}
+            PlayerAction::Message => {
+                if uid == self.creator.id {
+
+                    self.participant.as_ref().unwrap().sender.send(Ok(Message::text("Ping from opponent")))
+                        .expect("Failed to send message");
+                }
+                // participant's turn
+                else {
+                    self.creator.sender.send(Ok(Message::text("Ping from opponent")))
+                        .expect("Failed to send message");
+                }
+            }
+            _ => {
+                eprintln!("Action not found which is {:?}", action);
+            }
         }
     }
 
-    fn get_creator_cards_combination(&self) -> CardCombination {
-        // TODO ::: Make it work
-        CardCombination::NONE
+    pub fn join_game(
+        &mut self,
+        id: String, 
+        sender: mpsc::UnboundedSender<Result<Message, warp::Error>>
+    ) {
+        self.participant.replace(User::new(id, sender));
+
+        // TODO Start a game.
+    }
+}
+
+pub struct User {
+    pub id : String,
+    pub sender : mpsc::UnboundedSender<Result<Message, warp::Error>>,
+    pub stat: PlayerStat,
+}
+
+impl User {
+    pub fn new(
+        id: String, 
+        sender: mpsc::UnboundedSender<Result<Message, warp::Error>>,
+    ) -> Self {
+        Self {  
+            id,
+            sender,
+            stat: PlayerStat::new(),
+        }
     }
 
-    fn get_participant_cards_combination(&self) -> CardCombination {
-        // TODO ::: Make it work
-        CardCombination::NONE
+    // TODO
+    pub fn get_card_combination(&self) -> CardCombination {
+        CardCombination::None
     }
+
+    pub fn add_card(&mut self, card: Card) {
+        self.stat.cards.push(card);
+    }
+
+    pub fn bet(&mut self, amount: u32) {
+        self.stat.bet.replace(amount);
+    }
+}
+
+pub struct PlayerStat {
+    pub cash : u32,
+    pub bet : Option<u32>,
+    pub cards: Vec<Card>,
 }
 
 pub enum Turn {
@@ -92,20 +176,22 @@ pub enum Turn {
     PARTICIPANT,
 }
 
-pub struct PlayerStat {
-    pub cash : u32,
-    pub bet : u32,
-    pub cards: Vec<Card>,
+impl PlayerStat {
+    pub fn new() -> Self {
+        Self {  
+            cash: 0,
+            bet: None,
+            cards: vec![],
+        }
+    }
 }
 
 pub struct CardPool {
     pub cards : Vec<Card>,
-    rng: ThreadRng,
 }
 
 impl CardPool {
     pub fn new() -> Self {
-        let rng = thread_rng();
         let mut cards: Vec<Card> = vec![];
         for card_type in CardType::iter() {
             for number in 0..CARD_NUMBER {
@@ -114,48 +200,76 @@ impl CardPool {
         }
         Self {  
             cards,
-            rng,
         }
     }
 
     pub fn poll_card(&mut self) -> Option<Card> {
         if self.cards.len() == 0 {return None;}
 
-        let index = self.rng.gen_range(0..self.cards.len());
+        // TODO ::: 
+        // This is not necessarily a great optimization since creation of thread local
+        // generator is not lightoperation. 
+        let index = rand::thread_rng().gen_range(0..self.cards.len());
 
         Some(self.cards.remove(index))
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 pub struct Card {
     card_type: CardType,
     number: u8,
 }
 
-#[derive(Debug ,Clone, Copy, EnumIter)]
+#[derive(Debug ,Clone, Copy, EnumIter, Serialize, Deserialize)]
 pub enum CardType {
-    DIAMOND,
-    SPADE,
-    HEART,
-    CLOVER,
+    Diamond,
+    Spade,
+    Heart,
+    Clover,
 }
 
 pub enum CardCombination {
-    NONE,
-    FULLHOUSE,
-    DOUBLE,
-    TRIPLE,
-    STRAIGHT,
-    FLUSH,
-    SFLUSH,
-    RSFLUSH,
+    None,
+    Fullhouse,
+    Double,
+    Triple,
+    Straight,
+    Flush,
+    Sflush,
+    Rsflush,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PlayerAction {
+    Message,
+    Poll_Card,
+    Bet_Raise,
+    Bet_Call,
+    Fold,
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum PlayerAction {
-    POLLCARD,
-    BETRAISE,
-    BETCALL,
-    FOLD,
+pub enum ResponseType {
+    Card,
+    Message,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UserRequest {
+    pub action: PlayerAction,
+    pub value: Option<u32>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ServerResponse {
+    pub response_type: ResponseType,
+    pub value: ResponseValue,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum ResponseValue {
+    Message(String),
+    Card(Card),
+    Bet(u32),
 }
