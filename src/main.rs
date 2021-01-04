16902f67b655b77059dd70363b55aaa48dc5360f
육심현 <simoncreek@tutanota.com>
2021-01-04 23:27:48 +0900

    Made main file sorted and clean.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod models;
 mod handlers;
+mod routes;
 mod test;
 
 use std::sync::{Arc, Mutex};
@@ -7,18 +8,21 @@ use std::collections::HashMap;
 use warp::Filter;
 
 use crate::handlers::*;
+use crate::routes::*;
 
 #[tokio::main]
 async fn main() {
     // TODO ::: Refacotr Connections from type alias to proper struct 
     let conn = Connections::new(Mutex::new(HashMap::new()));
-    let ws = warp::path("echo")
-        // The `ws()` filter will prepare the Websocket handshake.
-        .and(warp::ws())
-        .and(with_conns(conn.clone()))
-        .and_then(ws_handler);
 
-    let routes = ws.with(warp::cors());
+    let create_room = routes::create_room(&conn);
+    //let get_rooms_list = routes::get_room();
+    let join_room = routes::join_room(&conn);
+
+    let routes = create_room
+        //.or(get_room)
+        .or(join_room)
+        .with(warp::cors());
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
