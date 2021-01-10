800e87eef219cf5cde790f20857ff5b6de8ac600
육심현 <simoncreek@tutanota.com>
2021-01-11 03:12:46 +0900

    Made some test modification

--- src/test.rs
+++ src/test.rs
@@ -1,26 +1,62 @@
-use crate::models;
+use crate::models::{CardPool, Card, CardType, CombinationBuilder};
+use rand::prelude::*;
 
 #[test]
 fn function_name_test() {
-    //let mut new_pool = models::CardPool::new();
+    //manual_comb_test();
+    //rand_comb_test();
+    //
+    
+    let abc = 15;
+    let def = 10;
 
-    //// TEST :: Print card pool
-    ////for item in new_pool.cards {
-        ////println!("{:?}", item);
-    ////}
-    ////
-    //// TEST :: Pool card and check if card was successfully deleted.
-    //println!("{:?}", new_pool.cards.len());
-    //let new_card = new_pool.poll_card();
+    println!("{:?}", abc.cmp(&def));
+}
 
-    //println!("{:?}", new_card);
-    //println!("{:?}", new_pool.cards.len());
+fn manual_comb_test() {
+    let mut cards: Vec<Card> = vec![
+        Card::new(CardType::Spade, 10),
+        Card::new(CardType::Diamond, 11),
+        Card::new(CardType::Heart, 12),
+        Card::new(CardType::Spade, 9),
+        Card::new(CardType::Spade, 8),
+        Card::new(CardType::Diamond, 7),
+    ];
 
+    let mut printer = cards.clone();
+    printer.sort_by(|a,b| a.number.cmp(&b.number));
 
-    // TEST ::: Serde_Json for option
-    let result = serde_json::to_string(&models::UserRequest{action: models::PlayerAction::MESSAGE, value: Some(25)});
-
-    if let Ok(content) = result {
-        println!("{:?}", content);
+    for item in printer {
+        println!("{:?}", item);
     }
+
+    let (highest, meta) = CombinationBuilder::get_highest_combination(cards);
+    println!("Highest combination is : {:?}, meta : {:?}", highest, meta);
+}
+
+fn rand_comb_test() {
+    let mut card_pool = CardPool::new();
+    let mut cards = vec![];
+    let mut cards_2 = vec![];
+    for _ in 0..4 {
+        let index = rand::thread_rng().gen_range(0..card_pool.cards.len());
+        cards.push( card_pool.cards.remove(index) );
+    }
+
+    for _ in 0..2 {
+        let index = rand::thread_rng().gen_range(0..card_pool.cards.len());
+        cards.push( card_pool.cards.remove(index) );
+    }
+
+    let mut merged = cards.iter().chain(cards_2.iter()).cloned().collect::<Vec<Card>>();
+
+    let mut printer = merged.clone();
+    printer.sort_by(|a,b| a.number.cmp(&b.number));
+
+    for item in printer {
+        println!("{:?}", item);
+    }
+
+    let ( highest , meta) = CombinationBuilder::get_highest_combination(merged);
+    println!("Highest combination is : {:?}, meta :{:?}", highest, meta);
 }
