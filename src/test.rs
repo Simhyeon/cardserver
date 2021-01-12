8ecc251251432347d0ccac6c26deef4a31fb8f0a
육심현 <simoncreek@tutanota.com>
2021-01-12 22:08:49 +0900

    Added env response and gameresult response handler + Made user log screen

--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,27 @@
 use crate::models::{CardPool, Card, CardType, CombinationBuilder};
 use rand::prelude::*;
+use rand::distributions::{Distribution, Uniform};
 
 #[test]
 fn function_name_test() {
     //manual_comb_test();
-    //rand_comb_test();
+    rand_comb_test();
     //
-    
-    let abc = 15;
-    let def = 10;
 
-    println!("{:?}", abc.cmp(&def));
+    //let mut array = vec![];
+    //let mut array_from = vec![1,2,3,4,5,6,7,8,9,10];
+    //let mut rng = rand::thread_rng();
+    //// TODO ::: 
+    //// This is not necessarily a great optimization since creation of thread local
+    //// generator is not lightoperation. 
+    //for _ in 0..3 {
+        //let between = Uniform::from(0..array_from.len());
+        //let index = between.sample(&mut rng);
+        ////let index = rand::thread_rng().gen_range(1..self.cards.len());
+        //array.push(array_from.remove(index));
+    //}
+
+    //println!("{:?}", array);
 }
 
 fn manual_comb_test() {
